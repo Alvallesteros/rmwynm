db1a0e5c78d9ebe273ed92b0a56eed4fa2dcd848
Alvallesteros <aj.alvallesteros@gmail.com>
2025-03-10 07:33:04 +0800

    Add: ToC

--- rmwynm/src/pages/Home.js
+++ rmwynm/src/pages/Home.js
@@ -6,10 +6,13 @@ const Home = () => {
   const [mood, setMood] = useState(null);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-orange-100 to-amber-100">
-      <h1 className="text-3xl font-bold text-white mb-4">READ ME WHEN YOU...</h1>
-      <FeelingSelector setMood={setMood} />
-      <MessageCard mood={mood} />
+    <div className="overflow-y-hidden">
+      <div className="relative min-h-screen flex items-center justify-center bg-cover bg-center fade-in-up" style={{ backgroundImage: "url('photos/Zyka-5.jpg')"}}>
+        <div className="absolute inset-0 bg-black opacity-50"></div> {/* Dimmed overlay */}
+        <div className="relative z-10 text-center text-white">
+          <h1 className="text-8xl drop-shadow-md font-bold mb-4">READ ME WHEN YOU...</h1>
+        </div>
+      </div>
     </div>
   );
 };
